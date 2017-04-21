9202099874895e7c73cf289f36dbc770e58ae01e
Jonah <amittai@techpandainc.com>
2017-04-21 17:00:11 +0000

    Commit #0006 (Disconnection handler)

--- src/main.py
+++ src/main.py
@@ -1,7 +1,15 @@
 import sys
 from server import Server
 
+def showHelp():
+    print("Server Help:")
+    print("./run [addr] [port]: Start the server on the specified address and port")
+
 def main():
+    if (len(sys.argv) < 3):
+        showHelp()
+        sys.exit(1)
+    
     server = Server(sys.argv[1], int(sys.argv[2]))
     server.start()
         
--- src/server.py
+++ src/server.py
@@ -14,7 +14,8 @@ class Server:
         self.sock.bind((addr, port))
         
         self.conns = []
-        
+    
+    # Checks server commands (separate thread)
     def commands(self):
         while True:
             cmd = raw_input("")
@@ -23,25 +24,44 @@ class Server:
                 os._exit(0)
             else:
                 print("Unknown command: '%s'" % cmd)
-                
+    
+    # Print to server log            
     def log(self, msg):
         print("%s %s" % (util.getTime(), msg))
-        
+    
+    # Send data to specified host/port    
     def sendData(self, msg, addr, port):
         self.sock.sendto(msg, (addr, port))
-        
+    
+    # Check if username exists    
     def nameExists(self, name):
         for conn in self.conns:
             if (conn.name == name):
                 return True
         return False
-        
+    
+    # Get username by IP and port    
     def getNameByIP(self, host, port):
         for conn in self.conns:
             if (conn.addr == host and conn.port == port):
                 return conn.name
         return None
     
+    # Get connection list index by username    
+    def getIndexByName(self, name):
+        i = 0
+        for conn in self.conns:
+            if (conn.name == name):
+                break
+            i += 1
+        return i
+    
+    # Broadcast to every connection    
+    def broadcast(self, msg):
+        for conn in self.conns:
+            self.sendData(msg, conn.addr, conn.port)
+    
+    # Main server loop
     def start(self):
         print("Starting server on %s:%d" % (self.addr, self.port))
         thread.start_new_thread(self.commands, ())
@@ -54,7 +74,7 @@ class Server:
                 args = data.split("~")
                 
                 #
-                #   /c/
+                #   /c/: Connection request
                 #
                 if (args[0] == "/c/"):
                     if (not self.nameExists(args[1])):
@@ -66,14 +86,21 @@ class Server:
                         self.sendData("/c/~BADNAME", host, port)
                     
                 #
-                #   /m/
+                #   /m/: User broadcast message
                 #    
                 elif (args[0] == "/m/"):
                     name = self.getNameByIP(host, port)
                     self.log("[%s]: %s" % (name, args[1]))
-                    for conn in self.conns:
-                        self.sendData("/m/~%s~%s" % (name, args[1]),
-                        conn.addr, conn.port)
+                    self.broadcast("/m/~%s~%s" % (name, args[1]))
+                        
+                #
+                #   /x/: Disconnection
+                #
+                elif (args[0] == "/x/"):
+                    name = self.getNameByIP(host, port)
+                    self.log("%s disconnected." % name)
+                    self.broadcast("/s/~%s disconnected." % name)
+                    del self.conns[self.getIndexByName(name)]
                     
             except KeyboardInterrupt:
                 break
